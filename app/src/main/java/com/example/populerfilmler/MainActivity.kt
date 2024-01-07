1eb353bdb47648b271b7e629ce33182d5d563790
Nasuh Unal <nasuhunal1@gmail.com>
2024-01-07 16:24:53 +0300

    update

--- app/src/main/java/com/example/populerfilmler/MainActivity.kt
+++ app/src/main/java/com/example/populerfilmler/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.populerfilmler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
